70a80057c6d7ae1b0ac7c3fe363b16cd11cd2b0f
lorenzo <99051451@mydavinci.nl>
2020-09-23 11:29:01 +0200

    lab added
    .

---
+++ lab/B1W5L1 - KaasFuncties/JavaScript/Logic.js
@@ -0,0 +1,35 @@
+if (prompt("is de kaas geel?") === "ja") {
+   if (prompt("zitten er gaten in?") ==="ja") {
+      if (prompt("is de kaas belachelijk duur?") ==="ja") {
+         alert("emmenthaler")
+      }else{
+         alert("leerdammer")
+         document.write("leerdammer")
+         document.write('<img src="img/leerdammer-original-half.jpg">')
+      }
+
+   }else{
+      if (prompt("is de kaas hard als steen?") ==="ja") {
+         alert("pamigiano reggiano")
+      }else{
+         alert("goudse kaas")
+      }
+   }
+
+}else{
+   if (prompt("heeft de kaas blauwe schimmel?") ==="ja") {
+      if (prompt("heeft de kaas een korst?") ==="ja") {
+         alert("bleu de rochbaron")
+      }else{
+         alert("foume d'Ambert")
+      }
+
+   }else{
+      if (prompt("heeft de kaas blauwe schimmel?") ==="ja") {
+         alert("camembert")
+         }else{
+         alert("mozzarella")
+      }
+
+   }
+}
